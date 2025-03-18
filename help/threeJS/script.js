4e6c0258f9e6d03422a527153dc5384e14e613aa
FTPyreNova024 <jdaniel.mc4@gmail.com>
2025-03-18 13:57:32 -0400

    added the files provided by the teacher

---
+++ help/threeJS/script.js
@@ -0,0 +1,256 @@
+document.body.style.cursor = 'none'
+/* three.js terrain */
+
+import * as THREE from 'three'
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+// https://threejs.org/examples/?q=pointerlock#misc_controls_pointerlock
+
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3(); //movement of camera
+const direction = new THREE.Vector3(); //direction
+const speed = 10;
+const moveSpeed =400;
+
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+const clock = new THREE.Clock();
+
+
+// Scene
+const scene = new THREE.Scene()
+
+// Camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+camera.position.y = 0;
+camera.position.x = 5;
+camera.position.z = 10;
+
+
+const renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.setAnimationLoop( animate );
+				document.body.appendChild( renderer.domElement );
+
+                const controls = new PointerLockControls(camera, renderer.domElement)
+
+
+
+scene.add( controls.object ); //add camera to the scene
+const onKeyDown = function ( event ) {
+    console.log(event.code);
+
+    switch ( event.code ) {
+
+       
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = true;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = true;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = true;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = true;
+            break;
+
+        case 'Space':
+            if ( canJump === true ) velocity.y += 350;
+            canJump = false;
+            break;
+
+            }
+
+        };
+
+        const onKeyUp = function ( event ) {
+
+            switch ( event.code ) {
+
+
+                case 'ArrowUp':
+                case 'KeyW':
+                    moveForward = false;
+                    break;
+
+                case 'ArrowLeft':
+                case 'KeyA':
+                    moveLeft = false;
+                    break;
+
+                case 'ArrowDown':
+                case 'KeyS':
+                    moveBackward = false;
+                    break;
+
+                case 'ArrowRight':
+                case 'KeyD':
+                    moveRight = false;
+                    break;
+
+            }
+
+        };
+
+        document.addEventListener( 'keydown', onKeyDown );
+        document.addEventListener( 'keyup', onKeyUp );
+
+
+        document.addEventListener( 'click', function () {
+
+            
+            controls.lock();
+
+        } );
+
+        controls.addEventListener( 'lock', function () {
+            console.log("locked");
+
+        } );
+
+
+// Models
+const gltfLoader = new GLTFLoader()
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+
+// Collision detection
+const objects = []
+
+let duckModel;
+
+gltfLoader.load(
+    'static/models/Duck/glTF/Duck.gltf',
+    (gltf) => {
+        duckModel = gltf.scene.children[0]
+        duckModel.position.set(1, 1, 0)
+        scene.add(duckModel)
+        objects.push(duckModel)
+    }
+)
+
+
+gltfLoader.load(
+    'static/models/Cube/cube.glb',
+    (gltf) => {
+        let cube = gltf.scene.children[0]
+        scene.add(cube)
+        objects.push(cube)
+    }
+)
+
+
+
+
+
+function movementUpdate(){
+    const time = performance.now();
+    if ( controls.isLocked === true ) {
+
+        // raycaster.ray.origin.copy( controls.object.position );
+        // raycaster.ray.origin.y -= 10;
+
+        // const intersections = raycaster.intersectObjects( objects, false );
+
+        //const onObject = intersections.length > 0;
+
+        const delta = ( time - prevTime ) / 1000;
+
+        velocity.x -= velocity.x * speed * delta;
+        velocity.z -= velocity.z * speed * delta;
+
+        velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+        direction.z = Number( moveForward ) - Number( moveBackward );
+        direction.x = Number( moveRight ) - Number( moveLeft );
+        direction.normalize(); // this ensures consistent movements in all directions
+
+        //update velocity z
+        if ( moveForward || moveBackward ) velocity.z -= direction.z * moveSpeed * delta;
+        //update velocity x
+        if ( moveLeft || moveRight ) velocity.x -= direction.x * moveSpeed * delta;
+
+        // if ( onObject === true ) {
+
+        //     velocity.y = Math.max( 0, velocity.y );
+        //     canJump = true;
+
+        // }
+
+
+        controls.moveRight( - velocity.x * delta );
+        controls.moveForward( - velocity.z * delta );
+
+        controls.object.position.y += ( velocity.y * delta ); // new behavior
+
+        if ( controls.object.position.y < speed ) {
+
+            velocity.y = 0;
+            controls.object.position.y = speed;
+
+            canJump = true;
+
+        }
+
+    }
+    prevTime = time;
+
+
+
+}
+// Floor
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#444444',
+        metalness: 0,
+        roughness: 0.5
+    })
+)
+floor.rotation.x = - Math.PI / 2
+scene.add(floor)
+objects.push(floor)
+
+// Animation loop
+function animate(){
+    if(controls.isLocked ===true){
+        movementUpdate();
+    }
+    renderer.render(scene, camera)
+}
---
+++ help/threeJS/scriptII.js
@@ -0,0 +1,112 @@
+document.body.style.cursor = 'none'
+/* three.js terrain 
+https://threejs.org/examples/?q=terra#webgl_geometry_terrain */
+
+import * as THREE from 'three'
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls.js'
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+
+
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+const clock = new THREE.Clock();
+
+
+// Scene
+const scene = new THREE.Scene()
+
+// Camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+camera.position.y = 0;
+camera.position.x = 5;
+camera.position.z = 10;
+camera.lookAt( -5 , 0, 10);
+
+
+const renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.setAnimationLoop( animate );
+				document.body.appendChild( renderer.domElement );
+
+
+const controls = new FirstPersonControls(camera, renderer.domElement)
+controls.lookSpeed = 0.01;
+controls.movementSpeed = 1;
+controls.noFly =false;
+controls.lookVertical = false;
+
+
+scene.add(camera); //add camera to the scene
+
+
+// Models
+const gltfLoader = new GLTFLoader()
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+
+// Collision detection
+const objects = []
+
+let duckModel;
+
+gltfLoader.load(
+    'static/models/Duck/glTF/Duck.gltf',
+    (gltf) => {
+        duckModel = gltf.scene.children[0]
+        duckModel.position.set(1, 1, 0)
+        //duckModel.position.set(1, 1, 0)
+        scene.add(duckModel)
+        objects.push(duckModel)
+    }
+)
+
+
+gltfLoader.load(
+    'static/models/Cube/cube.glb',
+    (gltf) => {
+        let cube = gltf.scene.children[0]
+        scene.add(cube)
+        objects.push(cube)
+    }
+)
+
+// Floor
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#444444',
+        metalness: 0,
+        roughness: 0.5
+    })
+)
+floor.rotation.x = - Math.PI / 2
+scene.add(floor)
+objects.push(floor)
+
+// Animation loop
+function animate(){
+    controls.update( clock.getDelta() );
+    renderer.render(scene, camera)
+}
+
