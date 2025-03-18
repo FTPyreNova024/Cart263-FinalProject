2b9c61364785cac3133e58459435e7e86848321f
FTPyreNova024 <jdaniel.mc4@gmail.com>
2025-03-18 19:38:23 -0400

    tried workingo n smt didnt work.

--- script.js
+++ script.js
@@ -1,61 +1,132 @@
+
 document.body.style.cursor = 'none'
 
+/* three.js terrain */
 import * as THREE from 'three'
-import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls.js'
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+// import * as CANNON from 'cannon-es';
+
+// https://threejs.org/examples/?q=pointerlock#misc_controls_pointerlock
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3(); //movement of camera
+const direction = new THREE.Vector3(); //direction
+const speed = 10;
+const moveSpeed = 300;
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
+const clock = new THREE.Clock();
+
 
 // Scene
 const scene = new THREE.Scene()
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(0, 1, 5)
-scene.add(camera)
-
-// Renderer
-const renderer = new THREE.WebGLRenderer({ canvas: canvas })
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.y = 0;
+camera.position.x = 5;
+camera.position.z = 10;
 
 
-// Controls
-const controls = new FirstPersonControls(camera, renderer.domElement)
-// Remove momentum from mouse look
-controls.lookSpeed = 0.1
-controls.lookVertical = true
-controls.constrainVertical = true
-controls.verticalMin = 1.0
-controls.verticalMax = 2.0
-controls.lon = 0
-controls.lat = 0
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
-controls.handleMouseMove = function (event) {
-    if (this.enabled === false) return
+const controls = new PointerLockControls(camera, renderer.domElement)
 
-    const movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0
-    const movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0
 
-    this.lon -= movementX * this.lookSpeed
-    this.lat -= movementY * this.lookSpeed
 
-    this.lat = Math.max(-85, Math.min(85, this.lat))
-    this.phi = THREE.MathUtils.degToRad(90 - this.lat)
-    this.theta = THREE.MathUtils.degToRad(this.lon)
+scene.add(controls.object); //add camera to the scene
+const onKeyDown = function (event) {
+    console.log(event.code);
 
-    const position = this.object.position
-    const targetPosition = new THREE.Vector3(
-        position.x + 100 * Math.sin(this.phi) * Math.cos(this.theta),
-        position.y + 100 * Math.cos(this.phi),
-        position.z + 100 * Math.sin(this.phi) * Math.sin(this.theta)
-    )
+    switch (event.code) {
 
-    this.object.lookAt(targetPosition)
-}
 
-document.addEventListener('mousemove', controls.handleMouseMove.bind(controls), false)
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = true;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = true;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = true;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = true;
+            break;
+
+        case 'Space':
+            if (canJump === true) velocity.y += 200;
+            canJump = false;
+            break;
+
+    }
+
+};
+
+const onKeyUp = function (event) {
+
+    switch (event.code) {
+
+
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = false;
+            break;
+
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = false;
+            break;
+
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = false;
+            break;
+
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = false;
+            break;
+
+    }
+
+};
+
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+
+
+document.addEventListener('click', function () {
+
+
+    controls.lock();
+
+});
+
+controls.addEventListener('lock', function () {
+    console.log("locked");
+
+});
+
 
 // Models
 const gltfLoader = new GLTFLoader()
@@ -90,52 +161,76 @@ gltfLoader.load(
         duckModel.position.set(1, 1, 0)
         scene.add(duckModel)
         objects.push(duckModel)
-        camera.position.set(1, 2, 3) // Adjust camera position relative to the duck
-        camera.lookAt(duckModel.position)
     }
 )
 
-const originalUpdate = controls.update.bind(controls)
-controls.update = (delta) => {
-    const prevPosition = camera.position.clone()
-    originalUpdate(delta)
-    if (duckModel) {
-        duckModel.position.copy(camera.position)
-        duckModel.position.y -= 1 // Adjust the height offset if needed
-        duckModel.rotation.y = camera.rotation.y // Rotate the duck model along with the camera
-    }
-    if (checkCollisions()) {
-        camera.position.copy(prevPosition)
-        if (duckModel) {
-            duckModel.position.copy(prevPosition)
-            duckModel.position.y -= 1 // Adjust the height offset if needed
-        }
-    }
-}
 
 gltfLoader.load(
     'static/models/Cube/cube.glb',
     (gltf) => {
         let cube = gltf.scene.children[0]
+        cube.scale.set(50, 50, 50);
         scene.add(cube)
         objects.push(cube)
     }
 )
 
-const checkCollisions = () => {
-    const cameraBox = new THREE.Box3().setFromObject(camera)
-    for (let i = 0; i < objects.length; i++) {
-        const objectBox = new THREE.Box3().setFromObject(objects[i])
-        if (cameraBox.intersectsBox(objectBox)) {
-            return true
-        }
-    }
-    return false
-}
+function movementUpdate() {
+    const time = performance.now();
+    if (controls.isLocked === true) {
 
+        // raycaster.ray.origin.copy( controls.object.position );
+        // raycaster.ray.origin.y -= 10;
+
+        // const intersections = raycaster.intersectObjects( objects, false );
+
+        //const onObject = intersections.length > 0;
+
+        const delta = (time - prevTime) / 1000;
+
+        velocity.x -= velocity.x * speed * delta;
+        velocity.z -= velocity.z * speed * delta;
+
+        velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+        direction.z = Number(moveForward) - Number(moveBackward);
+        direction.x = Number(moveRight) - Number(moveLeft);
+        direction.normalize(); // this ensures consistent movements in all directions
+
+        //update velocity z
+        if (moveForward || moveBackward) velocity.z -= direction.z * moveSpeed * delta;
+        //update velocity x
+        if (moveLeft || moveRight) velocity.x -= direction.x * moveSpeed * delta;
+
+        // if ( onObject === true ) {
+
+        //     velocity.y = Math.max( 0, velocity.y );
+        //     canJump = true;
+
+        // }
+
+
+        controls.moveRight(- velocity.x * delta);
+        controls.moveForward(- velocity.z * delta);
+
+        controls.object.position.y += (velocity.y * delta); // new behavior
+
+        if (controls.object.position.y < speed) {
+
+            velocity.y = 0;
+            controls.object.position.y = speed;
+
+            canJump = true;
+
+        }
+
+    }
+    prevTime = time;
+
+}
 // Floor
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
+    new THREE.PlaneGeometry(1000, 1000),
     new THREE.MeshStandardMaterial({
         color: '#444444',
         metalness: 0,
@@ -147,10 +242,10 @@ scene.add(floor)
 objects.push(floor)
 
 // Animation loop
-const animate = () => {
-    requestAnimationFrame(animate)
-    controls.update(0.1) // Update controls
+function animate() {
+    if (controls.isLocked === true) {
+        movementUpdate();
+        // updatePhysics();
+    }
     renderer.render(scene, camera)
 }
-
-animate()
